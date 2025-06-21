aeb0ca8215d97819c0a89ebaa2f88925d7bd5e8b
Ajaykumar-6209 <ajaygoud6333@gmail.com>
2025-06-21 04:16:21 +0000

    first commit

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        // Create a new instance of the LogicalBuilding class
+        LogicalBuilding building = new LogicalBuilding();
+
+        // Call the method to display the building's information
+        building.displayBuildingInfo();
+    }
+}
