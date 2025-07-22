9b9d71de513ebaae3bceb0fa5c579fe2e106865e
Ajaykumar-6209 <ajaygoud6333@gmail.com>
2025-07-22 05:36:55 +0000

    binary numbers

---
+++ binary.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class binary{
+    public static int binarySearch(int numbers[] , int target){
+        int start = 0;
+        int end = numbers.length-1;
+         
+       while(start <= end){
+            int mid =(start + end)/2;
+
+            if(numbers[mid] == target){
+                return mid;
+            }else if(numbers[mid] < target){
+                start = mid +1;
+            }else if(numbers[mid] > target) {
+                end = mid -1;
+            }
+       }
+              return -1;
+    }
+    public static void main(String args[]){
+        int numbers[] ={2,4,6,8,12,14,16};
+        int target = 8;
+        int key = binarySearch(numbers,target);
+        System.out.println(key);
+    }
+
+}
--- large.java
+++ large.java
@@ -3,19 +3,22 @@ public class large {
 
     public static int getLargest(int num[]){
         int largest = Integer.MIN_VALUE;
-        int small = Integer.MAX_VALUE;
+        int smallest = Integer.MAX_VALUE;
 
         for(int i = 0; i<num.length; i++){
             if(largest < num[i]){
                  largest = num[i];
-          
+             }
+             if(smallest > num[i]){
+                smallest = num[i];
              }
         }
+        System.out.println("get smallest number in array :" smallest);
         return largest;
 
     }
     public static void main(String args[]){
         int num[] = {2,4,6,8,10,12};
-        System.out.println("Get Largest num in array" + getLargest(num));
+        System.out.println("Get Largest num in array :" + getLargest(num));
     }
 }
